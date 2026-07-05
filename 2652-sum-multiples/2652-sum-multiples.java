7fc7e49983ac0482d1992357545a659b9e3160bb
Mukesh Ganesan <ganesanmukesh2121@gmail.com>
2026-07-05 23:18:53 +0530

    Time: 3 ms (95.36%), Space: 42.7 MB (42.58%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+        if(i%3==0||i%5==0||i%7==0){
+            sum += i;
+        }
+        }
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
