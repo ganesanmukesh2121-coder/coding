126cbed0753c4a2e3d5af1df202e15d335293059
Mukesh G <ganesanmukesh2121@gmail.com>
2026-08-01 14:29:46 +0530

    Time: 6 ms (18.04%), Space: 48.9 MB (15.14%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            int num = i;
+            int count = 0;
+
+            while (num > 0) {
+                count += num % 2;
+                num /= 2;
+            }
+
+            ans[i] = count;
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
