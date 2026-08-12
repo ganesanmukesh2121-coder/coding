b5665d2404124d6cf20f81104ed0ed2120120e5c
Mukesh G <ganesanmukesh2121@gmail.com>
2026-08-12 22:36:49 +0530

    Time: 0 ms (100%), Space: 48.4 MB (38.15%) - LeetHub - Time: O(n), Space: O(1)

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,18 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        //String n = "" ;
         int l = 0 ;
         int r = s.length - 1 ;
-
         while(l <= r){
           char temp = s[l] ;
           s[l] = s[r] ;
           s[r] = temp ;
- 
            l++;
            r--;
         }
-       // System.out.print(Arrays.toString(s)) ;
     }
 }
 
